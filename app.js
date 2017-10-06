5f2bef8a05e1572cb7efcd813841f35e04742a87
himharsh1997 <himharsh1997@gmail.com>
2017-10-06 21:04:16 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+angular.module('checkker',[])
+.controller('mycontrol',function($scope){
+$scope.name="";
+$scope.checkf=function(){
+ if(this.name==""||this.name==" ")
+ {
+   this.name="Please enter data first";
+  }
+else if(this.name!=""||this.name==" ")
+{
+    var x=this.name.split(",");
+    var l=x.length;
+     if(l>=3)
+     {
+      this.name="Enjoy!";
+      }  
+     else{
+         this.name="Too much!";
+       }
+ }
+
+}
+});
